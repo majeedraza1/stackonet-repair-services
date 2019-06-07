f56d9a9ae13c825989e847ee09fb79391d997a91
simpi <simpisingh21@gmail.com>
2019-06-07 11:31:33 +0530

    Add chat app design.

---
+++ assets/src/frontend/rent-a-center/pages/ChatApp.vue
@@ -0,0 +1,203 @@
+<template>
+    <div class="container">
+        <div class="messaging">
+            <div class="inbox_msg">
+                <div class="inbox_people">
+                    <div class="field has-addons">
+                        <div class="type_msg">
+                            <div class="input_msg_write">
+                                <input type="text" class="write_msg"
+                                       placeholder="Type Manager name OR store name to search"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="inbox_chat">
+                        <div class="chat_list active_chat">
+                            <div class="chat_people">
+                                <div class="chat_img">
+                                    <figure class="image is-48x48">
+                                        <img class="is-rounded"
+                                             src="//localhost:3000/wp-content/uploads/2019/06/img.jpg">
+                                    </figure>
+                                </div>
+                                <div class="chat_ib">
+                                    <h5>Domnic Brown<span class="chat_date">2 mins</span></h5>
+                                    <p>It is a long established
+                                        fact....</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat_list">
+                            <div class="chat_people">
+                                <div class="chat_img">
+                                    <figure class="image is-48x48">
+                                        <img class="is-rounded"
+                                             src="//localhost:3000/wp-content/uploads/2019/06/img.jpg">
+                                    </figure>
+                                </div>
+                                <div class="chat_ib">
+                                    <h5>Garry Sobars <span class="chat_date">10 mins</span></h5>
+                                    <p>English versions from the
+                                        1914...</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat_list">
+                            <div class="chat_people">
+                                <div class="chat_img">
+                                    <figure class="image is-48x48">
+                                        <img class="is-rounded"
+                                             src="//localhost:3000/wp-content/uploads/2019/06/img.jpg">
+                                    </figure>
+                                </div>
+                                <div class="chat_ib">
+                                    <h5>Jeson Born <span class="chat_date">25 mins</span></h5>
+                                    <p>>It is a long established
+                                        fact....</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat_list">
+                            <div class="chat_people">
+                                <div class="chat_img">
+                                    <figure class="image is-48x48">
+                                        <img class="is-rounded"
+                                             src="//localhost:3000/wp-content/uploads/2019/06/img.jpg">
+                                    </figure>
+                                </div>
+                                <div class="chat_ib">
+                                    <h5>John Smith<span class="chat_date">1 hour</span></h5>
+                                    <p>The standard chunk of Lorem
+                                        Ip....</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat_list">
+                            <div class="chat_people">
+                                <div class="chat_img">
+                                    <figure class="image is-48x48">
+                                        <img class="is-rounded"
+                                             src="//localhost:3000/wp-content/uploads/2019/06/img.jpg">
+                                    </figure>
+                                </div>
+                                <div class="chat_ib">
+                                    <h5>John Smith <span class="chat_date">1 day</span></h5>
+                                    <p>The standard chunk of Lorem
+                                        Ip....</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <nav class="navbar" role="navigation" aria-label="main navigation">
+                    <div id="navbarBasicExample" class="navbar-menu">
+                        <div class="navbar-start">
+                            <a class="navbar-item">
+                                <img class="is-rounded" src="//localhost:3000/wp-content/uploads/2019/06/img.jpg"
+                                     width="50" height="50">
+                            </a>
+                            <a class="navbar-item">
+                                Andrew andy
+                            </a>
+                            <a class="navbar-item">
+                                <div class="dot-right">
+                                    <span class="dot"></span>
+                                    <span class="dot"></span>
+                                    <span class="dot"></span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+                <hr class="navbar-divider">
+                <h6 class="time-title">April 20,2019 at 3:45 pm</h6>
+                <div class="mesgs">
+                    <div>
+                        <div class="incoming_msg">
+                            <div class="received_msg">
+                                <div class="received_withd_msg">
+                                    <p>English versions from the
+                                        1914...
+                                        <br>
+                                        <span class="time_date"> Tuesday</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="outgoing_msg">
+                            <div class="sent_msg">
+                                <p>There are many variations of passages of<br>
+                                    <span class="time_date-sent"> 3:12:36 PM</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="incoming_msg">
+
+                            <div class="received_msg">
+                                <div class="received_withd_msg">
+                                    <p>All the Lorem Ipsum generators on the<br>
+                                        <span class="time_date">3:12:45 PM</span>
+                                    </p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="outgoing_msg">
+                            <div class="sent_msg">
+                                <p>There are many variations of passages of
+                                    <br>
+                                    <span class="time_date-sent"> 3:13:04 PM</span></p>
+
+                            </div>
+                        </div>
+                        <div class="incoming_msg">
+
+                            <div class="received_msg">
+                                <div class="received_withd_msg">
+                                    <p>It is a long established fact<br>
+                                        <span class="time_date"> 3:11:25 PM</span>
+                                    </p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="outgoing_msg">
+                            <div class="sent_msg">
+                                <p>The standard chunk of Lorem Ipsum used
+                                    since the 1500s is reproduced below for those interested<br>
+                                    <span class="time_date-sent"> 3:11:25 PM</span>
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="type_msg">
+                        <div class="input_msg_write">
+                            <div id="fa-plus"><i class="fa">&#xf067;</i></div>
+                            <input id="input" type="text" class="write_msg" placeholder="Type a message"/>
+                                <icon id="button" class="msg_send_btn">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 30 35">
+                                    <path d="M17 32c-0.072 0-0.144-0.008-0.217-0.024-0.458-0.102-0.783-0.507-0.783-0.976v-15h-15c-0.469 0-0.875-0.326-0.976-0.783s0.129-0.925 0.553-1.123l30-14c0.381-0.178 0.833-0.098 1.13 0.199s0.377 0.749 0.199 1.13l-14 30c-0.167 0.358-0.524 0.577-0.906 0.577zM5.508 14h11.492c0.552 0 1 0.448 1 1v11.492l10.931-23.423-23.423 10.931z"></path>
+                                </svg>
+                                </icon>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import Icon from "../../../shapla/icon/icon";
+    export default {
+        name: "ChatApp",
+        components: {Icon},
+    }
+</script>
+<style lang="scss">
+
+
+</style>
--- assets/src/frontend/rent-a-center/routers.js
+++ assets/src/frontend/rent-a-center/routers.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Dashboard from './pages/Dashboard';
 import StoreAddress from './pages/StoreAddress';
+import ChatApp from './pages/ChatApp'
 
 Vue.use(VueRouter);
 
 const routes = [
 	{path: '/', name: 'Dashboard', component: Dashboard},
 	{path: '/store-addresses', name: 'StoreAddress', component: StoreAddress},
+	{path: '/chat-app', name: 'ChatApp', component: ChatApp}
 ];
 
 export default new VueRouter({
